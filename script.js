bd145b1c9473fe95d4f1775aec999c195f5faa3a
AKSHATNEMA <AKSHATNEMA@users.noreply.github.com>
2021-06-29 17:17:43 +0530

    updated whole project

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('hello!')
+var x = document.getElementsByClassName("collapsible1");
+var i;
+
+for (i = 0; i < x.length; i++) {
+  x[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
